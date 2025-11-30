ddde5c7fc767d642d6f820ca1088ecbb56cc5ae4
userNIGAM <nigam333nigam@gmail.com>
2025-11-30 23:44:52 +0545

    some features

--- attandance/src/App.jsx
+++ attandance/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import AttendanceSystem from "./components/AttendanceSystem";
+import QRCodeGenerator from "./components/qrcode/QRCodeGenerator";
 
 const App = () => {
   return (
     <>
       <AttendanceSystem />
+      <QRCodeGenerator />
     </>
   );
 };
